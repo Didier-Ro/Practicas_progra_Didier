d12b04650c5e3c651e8052ece5cfe40487de109e
Didier-Ro <85504003+Didier-Ro@users.noreply.github.com>
2021-09-05 13:35:26 -0500

    Proyecto

---
+++ helloworld-cc/src/main.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include "myheader.hh"
+
+int main()
+{
+    /*int numberInteger{};
+    float numberFloat{5.f};
+    char* stringChar{"My string char"};
+    char chararray[]{"char array"};
+    std:: string myString {"my string form stl"};
+
+    std:: cout<< "Suma " << Add ("Hello", "World")<<std::endl;
+    std::cout<<numberInteger<<std::endl;
+    std::cout<<numberFloat<< std:: endl;
+    std::cout<<stringChar<< std:: endl;
+    std::cout<<chararray<<std:: endl;
+    std::cout<<myString<<std:: endl;*/
+
+    
+
+    std:: cin.get();
+    return 0;
+}
