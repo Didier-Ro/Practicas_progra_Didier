0b7e0e1918444cfdddd8861ab5ec69acbddf507d
Didier-Ro <85504003+Didier-Ro@users.noreply.github.com>
2021-09-15 10:52:59 -0500

    Sprites

--- amerike-smfl-gameengine/src/Game.cc
+++ amerike-smfl-gameengine/src/Game.cc
@@ -1,8 +1,14 @@
 #include "Game.hh"
 #include "Constants.hh"
 #include "Rectangle.hh"
+#include "InputSystem.hh"
+#include <iostream>
 
-Rectangle* rectangle{new Rectangle(100, 100, 200, 100, sf::Color::Red)};
+Rectangle* rectangle{new Rectangle(100, 100, 300, 100, sf::Color::Red)};
+sf::Clock* gameClock{new sf::Clock()};
+float deltaTime{};
+sf::Texture* texture1{new sf::Texture()};
+sf::Sprite* sprite1{new sf::Sprite()};
 
 Game::Game()
 {
@@ -17,7 +23,13 @@ Game::~Game()
 //Starting things
 void Game::Start()
 {
-
+  texture1->loadFromFile("../assets/sprites.png");
+  if(texture1 != nullptr) std::cout << "Ok";
+  sprite1->setTexture(*texture1);
+  sprite1->setTextureRect(sf::IntRect(0 * 16, 5 * 16, 16, 16));
+  sprite1->setPosition(100, 25);
+  sprite1->setColor(sf::Color::White);
+  sprite1->setScale(4.f, 4.f);
 }
 
 void Game::Initialize()
@@ -29,7 +41,8 @@ void Game::Initialize()
 //Logic, animations, etc
 void Game::Update()
 {
-
+  deltaTime = gameClock->getElapsedTime().asSeconds();
+  gameClock->restart();
 }
 
 void Game::MainLoop()
@@ -61,12 +74,14 @@ void Game::Render()
 void Game::Draw()
 {
   window->draw(*rectangle->GetShape());
+  window->draw(*sprite1);
 }
 
 //Keyboard, joysticks, etc.
 void Game::Input()
 {
-
+  rectangle->GetShape()->move(sf::Vector2f(InputSystem::Axis().x * deltaTime * 1000.f,
+  InputSystem::Axis().y * deltaTime * 1000.f));
 }
 
 void Game::Destroy()
