1836cef4e69df84bf53e5ab0d47251a27302edcb
Didier-Ro <ortizrojeroh@gmail.com>
2021-10-20 11:01:03 -0500

    actual

--- amerike-smfl-gameengine/src/TileGroup.cc
+++ amerike-smfl-gameengine/src/TileGroup.cc
@@ -1,27 +1,21 @@
 #include "TileGroup.hh"
 #include<iostream>
 
-int size{4};
-
-TileGroup::TileGroup(sf::RenderWindow*& window,int COLS, int ROWS, const char* filePath, 
-float scale, float tileWidth, float tileHeigth, const char* textureUrl)
+TileGroup::TileGroup(sf::RenderWindow*& window, int COLS, int ROWS, const char* filePath, 
+float scale, float tileWidth, float tileHeight, const char* textureUrl)
 {
-    this->textureUrl = textureUrl;
-    this->tileWidth = tileWidth;
-    this->tileHeight = tileHeight;
-    this->scale = scale;
-    this->COLS = COLS;
-    this->ROWS = ROWS;
-    this->filePath = filePath;
-    reader = new std::ifstream();
-    this->window = window;
-    tiles = new std::vector<Tile*>();
-    /*std::string line;
-    while (std::getline(*reader, line))
-    {
-        std::cout << line << std::endl;
-    }*/
-    GenerateMap();
+  this->textureUrl = textureUrl;
+  this->tileWidth = tileWidth;
+  this->tileHeight = tileHeight;
+  this->scale = scale;
+  this->COLS = COLS;
+  this->ROWS = ROWS;
+  this->filePath = filePath;
+  reader = new std::ifstream();
+  this->window = window;
+  tiles = new std::vector<Tile*>();
+
+  GenerateMap();
 }
 
 TileGroup::~TileGroup()
@@ -31,20 +25,6 @@ TileGroup::~TileGroup()
 
 void TileGroup::GenerateMap()
 {
-  /*|Tile* tile1{new Tile("assets/tiles.png", 4.f, 16, 16, 0, 2, 0, 0, window)};
-  Tile* tile2{new Tile("assets/tiles.png", 4.f, 16, 16, 1, 2, 16 * 1 * 4.f, 0, window)};
-  Tile* tile3{new Tile("assets/tiles.png", 4.f, 16, 16, 2, 2, 16 * 2 * 4.f, 0, window)};
-  Tile* tile4{new Tile("assets/tiles.png", 4.f, 16, 16, 3, 2, 16 * 3 * 4.f, 0, window)};*/
-
-  /*for(int i{}; i < size - 1; i++)
-  {
-    tiles->push_back(new Tile("../assets/tiles.png", 4.f, 16, 16, i, 2, 16 * i * 4.f, 0, window));
-  }*/
-
-  /*tiles->push_back(tile1);
-  tiles->push_back(tile2);
-  tiles->push_back(tile3);
-  tiles->push_back(tile4);*/
   reader->open(filePath);
   for(int y{}; y < ROWS; y++)
   {
